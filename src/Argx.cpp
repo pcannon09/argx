4027ead4d5b8af7f5323dc959fb757493e72e8cf
pcannon09 <pcannon775@gmail.com>
2025-06-03 13:03:43 +0200

    Finished style  for Professional

--- src/Argx.cpp
+++ src/Argx.cpp
@@ -4,6 +4,8 @@
 
 #include <string>
 #include <iostream>
+#include <codecvt>
+#include <locale>
 
 #include "../inc/Argx.hpp"
 #include "../inc/types.hpp"
@@ -77,33 +79,62 @@ namespace argx
 		return result;
 	}
 
-	std::string Argx::createDoc(ARGXStyle style, std::string title, std::string mainInfo)
-	{
-		std::string startStr;
-		std::string contentStr;
-		std::string endStr;
 
-		contentStr += mainInfo;
+	
+	std::string Argx::createDoc(ARGXStyle style, const std::string &title, const std::string &mainInfo)
+	{
+		std::string contentStr;
 
 		if (style == ARGXStyle::Professional)
 		{
-			unsigned int i = 0;
-
 			for (const auto &x : this->options)
 			{
-				++i;
-
-				std::string subParamSpacing;
-
+				// Main option header line
 				contentStr += "ID: " + x.id + "\n";
-				contentStr += "[ " + std::string(x.sparam) + " | " + std::string(x.param) + 
-               		(x.hasSubParams ? x.subParams[i].param : std::string("")) + "] " + 
-               		std::string(x.info);
+				contentStr += "[ " + x.sparam + " | " + x.param;
+
+				if (x.hasSubParams && !x.subParams.empty())
+				{
+					contentStr += " [ ";
+
+					for (size_t i = 0; i < x.subParams.size(); ++i)
+					{
+						const auto &sub = x.subParams[i];
+
+						contentStr += sub.param;
+
+						if (i < x.subParams.size())
+							contentStr += " | ";
+					}
+
+					contentStr += "] ] ";
+				}
+
+				else contentStr += " ] ] ";
+
+				contentStr += x.info + "\n";
+
+				// Print all sub-options with sparam and param, aligned with ideographic spaces if there are
+				if (x.hasSubParams && !x.subParams.empty())
+				{
+					for (const auto &sub : x.subParams)
+					{
+						// Create ideographic spaces matching the length of main param for alignment
+						std::wstring wideSpaces(x.param.size(), L'\u3000');
+						std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+						std::string spacing = converter.to_bytes(wideSpaces);
+
+						contentStr += spacing + "  [ " + sub.sparam + " | " + sub.param + " ] " + sub.info + "\n";
+					}
+				}
 			}
 		}
-		
-		return "";
+
+		return title + "\n" + mainInfo + "\n" + contentStr;
 	}
+
+
+
 
 	std::vector<std::string> Argx::getMainArgs() const
 	{ return *this->mainArgs; }
