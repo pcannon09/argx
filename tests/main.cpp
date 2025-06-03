7c7ca19e1f0368b4cac9b74206f4b4d276f89bc1
pcannon09 <pcannon775@gmail.com>
2025-06-03 18:59:24 +0200

    Minor changes; Added version output that says 'VERSION' just for a simple test

--- tests/main.cpp
+++ tests/main.cpp
@@ -33,8 +33,14 @@ int main(int argc, char *argv[])
 		randomSubOption.id = "message";
 		randomSubOption.param = "msg";
 		randomSubOption.sparam = "m";
-		versionSubOption.info = "Show this message \"Hello world\"";
+		randomSubOption.info = "Show this message \"Hello world\"";
 		randomSubOption.hasSubParams = true;
+
+		randomSubOption.id = "other";
+		randomSubOption.param = "other";
+		randomSubOption.sparam = "o";
+		randomSubOption.info = "Show this message \"Goodbye world\"";
+		randomSubOption.hasSubParams = false;
 
 		mainOptions.subParams.push_back(versionSubOption);
 		mainOptions.subParams.push_back(randomSubOption);
@@ -43,9 +49,18 @@ int main(int argc, char *argv[])
 		docStr = mainArgx.createDocs(argx::ARGXStyle::Professional, "-- Docs ----", "This is the main documentation for ARGX");
 	}
 
-	if (mainArgx.getParam("help").exists)
+	if (mainArgx.getParam("help").exists && mainArgx.findParam("help"))
 	{
-		std::cout << docStr << "\n";
+		argx::ARGXParam helpParam = mainArgx.getParam("help");
+
+    	if (mainArgx.paramExists("version") && helpParam.subExists[mainArgx.findParam("version")])
+    	{
+        	std::cout << "VERSION\n";
+
+        	return 0;
+    	}
+
+    	std::cout << docStr << "\n";
 	}
 
 	return 0;
