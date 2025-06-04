a14b76e614a8c7d1542a58bf030050b9885ce1f8
pcannon09 <pcannon775@gmail.com>
2025-06-04 19:12:53 +0200

    Updated project version to 1.0.0-build

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 // Versioning
-#define ARGX_VERSION_MAJOR            0
-#define ARGX_VERSION_MINOR            1
-#define ARGX_VERSION_PATCH            2
+#define ARGX_VERSION_MAJOR            1
+#define ARGX_VERSION_MINOR            0
+#define ARGX_VERSION_PATCH            0
 
-#define ARGX_VERSION_STD              20250604 // Once released, it will be YyyyMmDd (eg: 20250604)
+#define ARGX_VERSION_STD              20250604
 
 // Version states:
 // * dev
 // * beta
 // * snap
 // * build (AKA: Release)
-#define ARGX_VERSION_STATE          "beta"
+#define ARGX_VERSION_STATE          "build"
 
 #define ARGX_VERSION                ((ARGX_VERSION_MAJOR<<16)|(ARGX_VERSION_MINOR<<8)|(ARGX_VERSION_PATCH)|(ARGX_VERSION_STATE << 24))
 
