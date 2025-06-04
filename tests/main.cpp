fff7ebe029b35aee5ae10d499b13e51b59a79bde
pcannon09 <pcannon775@gmail.com>
2025-06-04 08:26:48 +0200

    Finished testing, made text more formal

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include "../inc/macros.hpp"
+
 #include "../inc/Argx.hpp"
 
 int main(int argc, char *argv[])
@@ -11,6 +13,8 @@ int main(int argc, char *argv[])
 	std::string docStr;
 	
 	argx::Argx mainArgx("main-args", argc, argv);
+
+	// Setup mainArgx
 	{
 		argx::ARGXOptions helpOption;
 		argx::ARGXOptions versionOption;
@@ -24,7 +28,7 @@ int main(int argc, char *argv[])
 		versionOption.id = "version";
 		versionOption.param = "--version";
 		versionOption.sparam = "-v";
-		versionOption.info = "Show fake version message";
+		versionOption.info = "Show version message";
 		versionOption.hasSubParams = true;
 		
 		argx::ARGXOptions versionSubOption;
@@ -53,34 +57,28 @@ int main(int argc, char *argv[])
 		docStr = mainArgx.createDocs(argx::ARGXStyle::Professional, "-- Docs ----", "This is a simple test for documentation using ARGX");
 	}
 
-	auto a = mainArgx.getOptions();
-
-	for (const auto &x : a)
-	{
-		std::cout << x.id << "\n";
-	}
-	
 	// Check if help exists
-	if (mainArgx.getParam("help").exists)
+	if (mainArgx.getArgc() <= 1) std::cout << docStr << "\n";
+
+	else if (mainArgx.getParam("help").exists)
 	{
 		argx::ARGXParam helpParam = mainArgx.getParam("help");
 
     	if (mainArgx.getSubParam(helpParam, "version"))
 		{
-			std::cout << "VERSION PARAM CALLED\n";
-			return 0;
+			std::cout << "For more information, call the following parameter: `--version`\n";
+
+			return 2;
 		}
 
     	else if (mainArgx.getSubParam(helpParam, "message"))
 		{
-			std::cout << "This is a random hard coded message string\n";
-			
-			if (mainArgx.getArgc() > 3) std::cout << "And this is your message: " << mainArgx.getMainArgs()[3] << "\n";
+			if (mainArgx.getArgc() > 3) std::cout << mainArgx.getMainArgs()[3] << "\n";
+			else std::cout << "Enter a message in the third parameter as a string" << "\n";
 
 			return 0;
 		}
 
-		// Default help output
 		std::cout << docStr << "\n";
 
 		return 0;
@@ -88,7 +86,17 @@ int main(int argc, char *argv[])
 
 	else if (mainArgx.getParam("version").exists)
 	{
-		std::cout << "Fake version, WOMP WOMP\n";
+		std::cout << "ARGX Version information:\n";
+		std::cout << "Version: " << std::to_string(ARGX_VERSION_MAJOR) << "." << std::to_string(ARGX_VERSION_MINOR) << "." << std::to_string(ARGX_VERSION_PATCH) << " " << ARGX_VERSION_STATE << "\n";
+		std::cout << "Version Standard: " << std::to_string(ARGX_VERSION_STD) << "\n";
+		std::cout << "Development Type: " << ARGX_DEV << "\n";
+	}
+	
+	else
+	{
+		std::cout << "Argx: Unknown option `" + (mainArgx.getArgc() > 1 ? mainArgx.getMainArgs()[1] : "<UNKNOWN>") + "`\n";
+
+		return 1;
 	}
 
 	return 0;
