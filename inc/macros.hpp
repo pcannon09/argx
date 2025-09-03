5c619419786ed0ea45aab4418796e855669087bd
pcannon09 <pcannon775@gmail.com>
2025-09-03 22:17:39 +0200

    Set build version

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -19,9 +19,9 @@
 // * snap                   2
 // * build (AKA: Release)   3
 #ifndef ARGX_AS_PYTHON_PACKAGE
-#   define ARGX_VERSION_STATE           "beta"
+#   define ARGX_VERSION_STATE           "build"
 #else
-#   define ARGX_VERSION_STATE           1
+#   define ARGX_VERSION_STATE           3
 #endif
 
 #define ARGX_VERSION                ((ARGX_VERSION_MAJOR<<16)|(ARGX_VERSION_MINOR<<8)|(ARGX_VERSION_PATCH)|(ARGX_VERSION_STATE << 24))
